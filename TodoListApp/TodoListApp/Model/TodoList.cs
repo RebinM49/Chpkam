926507e7e3d7599f8ba5fcb8c28d147ca2a757ff
Rebin <rebinm499@gmail.com>
2023-09-10 18:45:13 +0330

    Data attribute added to Id property of model class

--- TodoListApp/TodoListApp/Model/TodoList.cs
+++ TodoListApp/TodoListApp/Model/TodoList.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,6 +9,8 @@ namespace TodoListApp.Model
 {
     public class TodoList
     {
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
         public string Name { get; set; }
         public String Description { get; set; }
         public List<TodoItem> Tasks { get; set; }
--- TodoListApp/TodoListApp/Model/TodoTask.cs
+++ TodoListApp/TodoListApp/Model/TodoTask.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,6 +9,7 @@ namespace TodoListApp.Model
 {
     public class TodoItem
     {
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int Id { get; set; }
         public string Title { get; set; }
         public bool Done { get; set; }
