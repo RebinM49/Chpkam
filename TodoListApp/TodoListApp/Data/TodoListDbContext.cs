29d661e32d3cd8b8fbdd1b839ce75b30ed77576c
Rebin <rebinm499@gmail.com>
2023-09-10 20:02:36 +0330

    DbContext class created

---
+++ TodoListApp/TodoListApp/Data/TodoListDbContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TodoListApp.Model;
+
+namespace TodoListApp.Data
+{
+    public class TodoListDbContext:DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            var connectionString =
+            optionsBuilder.UseSqlite($"Data Source={AppDomain.CurrentDomain.BaseDirectory}ClubMembershipDb.db");
+            base.OnConfiguring(optionsBuilder);
+        }
+
+        public DbSet<TodoList> TodoList { get; set; }
+        public DbSet<TodoItem> TodoItem { get; set; }
+    }
+}
