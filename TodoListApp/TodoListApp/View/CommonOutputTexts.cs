e142f837ee01ce08f211c345764ed7fb523b25a7
Rebin <rebinm499@gmail.com>
2023-09-11 10:31:38 +0330

    CommonOutputText Class For Generate Heading ... created

---
+++ TodoListApp/TodoListApp/View/CommonOutputTexts.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TodoListApp.View
+{
+    public static class CommonOutputTexts
+    {
+        public static void GenerateHeading (string text)
+        {
+            
+            var heading = $"ToDo App \n {text} \n";
+            var border = new String('-',heading.Length );
+            Console.WriteLine($"{border}\n{heading}{border}");
+        }
+    }
+}
