7d34f8be24f4726c5d5fe60e76cec2daa7f26949
Rebin <rebinm499@gmail.com>
2023-09-12 19:38:09 +0330

    IToDolist interface works as DTO class

---
+++ TodoListApp/TodoListApp/Model/IToDoList.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TodoListApp.Model
+{
+    internal interface IToDoList
+    {
+        public string Title { get; set; }
+        public bool Open { get; set; }
+        public String? Description { get; set; }
+    }
+}
