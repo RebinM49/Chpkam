9280cb636bbf0553fd7f9af0f9aefb5d05caea3d
Rebin <rebinm499@gmail.com>
2023-09-17 07:12:06 +0330

    The IlistView and listview which related to List UI are created

---
+++ TodoListApp/TodoListApp/View/IListView.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TodoListApp.View
+{
+    public interface IListView
+    {
+        public void DisplayListSection();
+        public void DisplayDeleteSection();
+        public void DisplayUpdateSection();
+        public void DisplayListsSection();
+    }
+}
---
+++ TodoListApp/TodoListApp/View/ListView.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using TodoListApp.Data;
+using TodoListApp.Model;
+
+namespace TodoListApp.View
+{
+    public class ListView :  IListView
+    {
+        IRegisterList _registerList;
+
+        public ListView(IRegisterList registerList)
+        {
+            _registerList = registerList;
+        }
+        public static void RunView()
+        {
+            CommonOutputTexts.GenerateHeading("List Menu");
+        }
+
+        public void DisplayDeleteSection()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DisplayListSection()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DisplayUpdateSection()
+        {
+            throw new NotImplementedException();
+        }
+
+
+        public void DisplayListsSection()
+        {
+            //Console.BackgroundColor = ConsoleColor.DarkGreen;
+            var lists = _registerList.GetAllList();
+            var sublist = lists.Select(x => x.Tasks);
+            foreach (var list in lists)
+            {
+                
+                if (list.Open)
+                {
+                    Console.WriteLine($"Title : {list.Title}\t Description : {list.Description}" +
+                    $"\t{list.Tasks.First<ToDoItem>().Title}");
+                }
+                
+                
+            }
+            Console.ResetColor();
+        }  
+    }
+}
