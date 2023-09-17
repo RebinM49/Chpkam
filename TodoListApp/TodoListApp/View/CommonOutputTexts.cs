294109e89e4f19f177687093de570eb1b74926a8
Rebin <rebinm499@gmail.com>
2023-09-17 07:11:09 +0330

    DisplayOperationMenu Added to commonotput class

--- TodoListApp/TodoListApp/View/CommonOutputTexts.cs
+++ TodoListApp/TodoListApp/View/CommonOutputTexts.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -14,6 +15,24 @@ namespace TodoListApp.View
             var heading = $"ToDo App \n {text} \n";
             var border = new String('-',heading.Length );
             Console.WriteLine($"{border}\n{heading}{border}");
+        }
+
+        
+
+        public static void DisplayOperationMenu()
+        {
+            Console.BackgroundColor = ConsoleColor.DarkBlue;
+            Console.WriteLine("Operations : \n");
+            Console.ResetColor();
+
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine($"[1] Create new List \t [2] Show ToDo Lists \t [3] Close a List \t [4] Update existing list");
+            Console.ResetColor();
+
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("Choose Operation type : ");
+            Console.ResetColor();
+            
         }
     }
 }
