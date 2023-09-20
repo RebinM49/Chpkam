1101b4feb906c2e644a2634b2f2bce8cbc87f0c2
Rebin <rebinm499@gmail.com>
2023-09-20 16:24:54 +0330

    ToEnum extenstion method created

---
+++ TodoListApp/TodoListApp/View/Abstraction/ExtensionMethods.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TodoListApp.View.Abstraction
+{
+    public static class ExtensionMethods
+    {
+        public static T ToEnum<T>(this string text, T defaultValue) where T : struct
+        {
+            if (string.IsNullOrEmpty(text))
+            {
+                return defaultValue;
+            }
+            T convertedValue;
+            return Enum.TryParse<T>(text, true, out convertedValue) ? convertedValue : defaultValue;
+        }
+    }
+}
