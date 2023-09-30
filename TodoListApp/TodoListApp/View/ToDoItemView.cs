9001a2a82b8690a53e070d3389f3b6ae5f493cda
Rebin <rebinm499@gmail.com>
2023-09-30 12:22:05 +0330

    Error handling mechanisms added to ToDoTask class

--- TodoListApp/TodoListApp/View/ToDoItemView.cs
+++ TodoListApp/TodoListApp/View/ToDoItemView.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 using TodoListApp.Data;
@@ -21,49 +22,55 @@ namespace TodoListApp.View
         {
             CommonOutputTexts.App_UI_headings("ToDo Item Menu");
         }
+
         public void DisplayAddSectionUI()
         {
             RunView();
             var item = new ToDoTask();
-            CommonOutputTexts.Write_UI_Title("Enter Title");
-            item.Title = Console.ReadLine();
+            item.Title = CommonInputTexts.GetStringInput("Enter Title");
+            item.EstimatedTime = CommonInputTexts.GetIntInput("Enter estimated Time");
+            item.Priority = CommonInputTexts.GetStringInput("Enter task priority ( low - medium - high")
+                .ToEnum<Importance>(Importance.Medium);
+            item.ToDolistId = CommonInputTexts.GetIntInput("Enter parent list id");
+            AddItem(item);
 
-            CommonOutputTexts.Write_UI_Title("Enter estimated Time");
-            int estimatedTime;
-            var estimatedInput = Console.ReadLine();
-            int.TryParse(estimatedInput, out estimatedTime);
-
-            CommonOutputTexts.Write_UI_Title("Enter task priority");
-            var priorityValue = Console.ReadLine();
-            priorityValue.ToEnum<Importance>(Importance.Medium);
-
-            CommonOutputTexts.Write_UI_Title("Enter parent list id");
-            int parentid;
-            var parentIdValue = Console.ReadLine();
-            int.TryParse(parentIdValue, out parentid);
-            item.ToDolistId = parentid;
-            _repository.Add(item);
         }
+        private void AddItem(ToDoTask item)
+        {
+            try
+            {
+                _repository.Add(item);
+                CommonOutputTexts.Write_system_messages("New Task Added , GoodLuck");
+            }
+            catch (Exception e)
+            {
+                CommonOutputTexts.Write_system_messages("The parent id you entered dose not exists");
+                CommonOutputTexts.Write_system_messages(e.Message);
 
+            }
+        }
+        /// <summary>
+        /// Generates the UI for show all task related to specific list
+        /// </summary>
         public void DisplayAllSectionUI()
         {
-
             CommonOutputTexts.App_UI_headings("This is your ToDo Lists");
+
             var items = _repository.GetAll();
             foreach (var item in items)
             {
-                if (!item.Done)
+                if (item.Done is false)
                 {
                     CommonOutputTexts.Write_UI_Title("Parent Id");
-                    CommonOutputTexts.Write_UI_values($"{item.Id}");
-                    CommonOutputTexts.Write_UI_Title("Id");
-                    CommonOutputTexts.Write_UI_values($"{item.Id}");
-                    CommonOutputTexts.Write_UI_Title("Title");
-                    CommonOutputTexts.Write_UI_values($"{item.Title}");
-                    CommonOutputTexts.Write_UI_Title("Estimated Time");
-                    CommonOutputTexts.Write_UI_values($"{item.EstimatedTime}");
-                    CommonOutputTexts.Write_UI_Title("Priotiry");
-                    CommonOutputTexts.Write_UI_values($"{item.Priority}");
+                    CommonOutputTexts.Write_UI_values($"\t{item.ToDolistId}");
+                    CommonOutputTexts.Write_UI_Title("\tId");
+                    CommonOutputTexts.Write_UI_values($"\t{item.Id}");
+                    CommonOutputTexts.Write_UI_Title("\tTitle");
+                    CommonOutputTexts.Write_UI_values($"\t{item.Title}");
+                    CommonOutputTexts.Write_UI_Title("\tEstimated Time");
+                    CommonOutputTexts.Write_UI_values($"\t{item.EstimatedTime}");
+                    CommonOutputTexts.Write_UI_Title("\tPriotiry");
+                    CommonOutputTexts.Write_UI_values($"\t{item.Priority}");
                 }
                 Console.WriteLine("\n");
             }
@@ -71,43 +78,98 @@ namespace TodoListApp.View
 
         public void DisplayDeleteSectionUI()
         {
-            throw new NotImplementedException();
-        }
 
+        }
+        /// <summary>
+        /// Generates the UI for Updateing a Task 
+        /// </summary>
         public void DisplayUpdateSecionUI()
         {
             CommonOutputTexts.App_UI_headings("Task Completion Status");
-            CommonOutputTexts.Write_UI_Title("Eneter the Id of related Task");
-            string inputId = Console.ReadLine();
-            int id;
-            int.TryParse(inputId, out id);
-            var task = _repository.GetById(id);
-            CommonOutputTexts.Write_UI_Title("Update Task status");
-            string status = Console.ReadLine().ToLower();
-            task.Done = status == "done" ? true : false;
-            _repository.Update(task);
-            CommonOutputTexts.Write_system_messages("Task Updated");
-            DisplayAllSectionUI();
+
+            var task = GetToDoTask();
+            if (task is not null)
+            {
+                var status = CommonInputTexts.GetStringInput("Enter status ( done - undone )");
+                task.Done = CheckforDone(status);
+                UpdateTask(task);
+            }
+            else
+            {
+                CommonOutputTexts.Write_system_messages("Id was not correct ! :)");
+            }
         }
 
+        /// <summary>
+        /// recives user input string and checks if value is "done" or not.just check for "done" .
+        /// </summary>
+        /// <param name="status">user input string</param>
+        /// <returns></returns>
+        private bool CheckforDone(string status)
+        {
+            if (status == "done")
+                return true;
+            return false;
+        }
+
+        /// <summary>
+        /// Recives a TodoTask object and update the repository if possible.Shows realated error !
+        /// </summary>
+        /// <param name="task">Object of type ToDoTask to update database with.</param>
+        private void UpdateTask(ToDoTask task)
+        {
+            try
+            {
+                _repository.Update(task);
+                CommonOutputTexts.Write_system_messages("Task Updated");
+            }
+            catch (Exception e)
+            {
+                CommonOutputTexts.Write_system_messages("This task cannot be Updated !");
+                CommonOutputTexts.Write_system_messages($"More detail :\n {e.Message} ");
+            }
+        }
+
+        /// <summary>
+        /// Display details of a specific Task . 
+        /// </summary>
         public void DisplaySingleItemSectionUI()
         {
             CommonOutputTexts.App_UI_headings("Task's Detail");
-            var item = GetToDoItem();
-            CommonOutputTexts.Write_UI_Title("Task Id");
-            CommonOutputTexts.Write_UI_values(item.Id.ToString());
-            CommonOutputTexts.Write_UI_Title("Task Status");
-            CommonOutputTexts.Write_UI_values(item.Done.ToString());
+            var item = GetToDoTask();
+            if (item is not null)
+            {
+                CommonOutputTexts.Write_UI_Title("Task Id");
+                CommonOutputTexts.Write_UI_values(item.Id.ToString());
+                CommonOutputTexts.Write_UI_Title("Task Title");
+                CommonOutputTexts.Write_UI_values($"{item.Title}");
+                CommonOutputTexts.Write_UI_Title("Task Priority");
+                CommonOutputTexts.Write_UI_values($"{item.Priority}");
+                CommonOutputTexts.Write_UI_Title("Task EstimatedTime");
+                CommonOutputTexts.Write_UI_values($"{item.EstimatedTime}");
+                CommonOutputTexts.Write_UI_Title("Task Status");
+                CommonOutputTexts.Write_UI_values(item.Done.ToString());
+            }
+            CommonOutputTexts.Write_system_messages("This task dose not exists !");
         }
-        private ToDoTask GetToDoItem()
+        /// <summary>
+        /// Get Task Id from user. Get related Task from repository if exists. 
+        /// </summary>
+        /// <returns>ToDoTask with given Id or Null</returns>
+        private ToDoTask GetToDoTask()
         {
-            CommonOutputTexts.Write_UI_Title("Enter Task's related Id");
-            string inputId = Console.ReadLine();
-            int id;
-            int.TryParse(inputId, out id);
-            var task = _repository.GetById(id);
+            int id = CommonInputTexts.GetIntInput("Enter Task's related Id");
+            ToDoTask task = null;
+            try
+            {
+                task = _repository.GetById(id);
+            }
+            catch (Exception ex)
+            {
+                CommonOutputTexts.Write_system_messages("Id is invalid");
+                CommonOutputTexts.Write_system_messages($"More detail :\n {ex.Message}");
+            }
             return task;
         }
-
     }
 }
