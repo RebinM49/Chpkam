dbd7e060d38cd9cc24bdb128c86dbf0a8fb311da
Rebin <rebinm499@gmail.com>
2023-09-30 12:19:44 +0330

    static CommonInputTexts class is created to get user input values

---
+++ TodoListApp/TodoListApp/View/Abstraction/CommonInputTexts.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TodoListApp.View.Abstraction
+{
+    public static class CommonInputTexts
+    {
+        public static string GetStringInput(string outputMessage)
+        {
+            CommonOutputTexts.Write_UI_Title(outputMessage);
+            var input = Console.ReadLine();
+            if (string.IsNullOrEmpty(input))
+            {
+                CommonOutputTexts.Write_system_messages("Enter a valid value");
+                GetStringInput(outputMessage);
+            }
+            return input;
+        }
+
+        public static int GetIntInput(string outputMessage)
+        {
+            CommonOutputTexts.Write_UI_Title(outputMessage);
+            string input = Console.ReadLine();
+
+            int convertedValue;
+            bool isNumber = int.TryParse(input, out convertedValue);
+            if (string.IsNullOrEmpty(input) || !isNumber)
+            {
+                GetIntInput(outputMessage);
+            }
+            return convertedValue;
+
+        }
+    }
+}
